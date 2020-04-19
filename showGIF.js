b28bf3e42dc631a160880620fddb1b3aa37b73a4
Anna Kasunic <amkasunic@gmail.com>
2020-04-19 15:03:47 -0400

    built basic join game functionality

--- showGIF.js
+++ showGIF.js
@@ -1,31 +1,30 @@
 
-
+//need to make sure not already showing a gif
+//this doesn't protect against multiple players choosing same site, might want to think about later
 if(document.querySelector('#foundGIF') == null){
-console.log('showing gif');
-console.log(gif_link);
-var foundGIF = document.createElement("img");
-foundGIF.src = gif_link;
-foundGIF.setAttribute("class", "found-player-gif");
-// foundGIF.id = "sampleGIF";
-foundGIF.style.position = "fixed";
-foundGIF.id = "foundGIF";
-foundGIF.style.top = "0px";
-foundGIF.style.left = "0px";
-document.querySelector('body').appendChild(foundGIF);
-foundGIF.onload= function(){
+	var foundGIF = document.createElement("img");
+	foundGIF.src = gif_link;
+	foundGIF.setAttribute("class", "found-player-gif");
+	// foundGIF.id = "sampleGIF";
+	foundGIF.style.position = "fixed";
+	foundGIF.id = "foundGIF";
+	foundGIF.style.top = "0px";
+	foundGIF.style.left = "0px";
+	document.querySelector('body').appendChild(foundGIF);
+	foundGIF.onload= function(){
 
-	height = foundGIF.height;	
-	width = foundGIF.width;
+		height = foundGIF.height;	
+		width = foundGIF.width;
 
 
-// Will execute myCallback every 0.5 seconds 
-	var intervalID = window.setInterval(myCallback, 1000);
-}
+	// Will execute myCallback every 0.5 seconds 
+		var intervalID = window.setInterval(myCallback, 1000);
+	}
 
-function myCallback() {
-	// console.log('callback');
- 	foundGIF.style.left = (Math.floor(Math.random()*(window.innerWidth-width))).toString() + "px";
- 	foundGIF.style.top = (Math.floor(Math.random()*(window.innerHeight-height))).toString() + "px";
-}
+	function myCallback() {
+		// console.log('callback');
+	 	foundGIF.style.left = (Math.floor(Math.random()*(window.innerWidth-width))).toString() + "px";
+	 	foundGIF.style.top = (Math.floor(Math.random()*(window.innerHeight-height))).toString() + "px";
+	}
 }
 
